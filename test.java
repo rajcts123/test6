e010c881d3ab8dda4def36de3f2e090c1eb43abc
rajcts123 <57384368+rajcts123@users.noreply.github.com>
2019-11-27 19:02:46 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,20 @@
+package devops;
+
+public class test {
+
+	public static void addTwoNumbers() {
+		// TODO Auto-generated constructor stub
+		int a=5,b=6;
+		int c =a+b;
+		System.out.println("addition of two numbers:" +c);
+					
+		
+	}
+
+	public static void main(String[] args) {
+				// TODO Auto-generated method stub
+		addTwoNumbers();
+
+	}
+
+}
